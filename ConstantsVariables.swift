726cd195806029a2368bf3b935f129d4a384e28c
Navinda Lankesh <adilanga@gmail.com>
2021-11-03 18:55:16 +0530

    Constants & Variables

---
+++ ConstantsVariables.swift
@@ -0,0 +1,24 @@
+// Stores value that can change at any time
+// Declare variable with var keyword
+var numberOfRows = 30
+
+// Stores value that never changes
+// Declare constant with let keyword
+let maxNumberOfRows = 100
+
+// count inferred to be of type Int
+let count = 10
+
+// price inferred to be of type Double
+var price = 23.55
+
+// myMessage inferred to be of type String
+var myMessage = "Swift is the future!"
+
+// Explicitly specifying type information
+// {var/let} <variable> : <type> = [ <value> ]
+var myMessage : String = "Swift is the future!"
+
+// Multiple variable declaration
+var x = 0, y = 1, z = 2
+var number1, number2, number3 : Double
