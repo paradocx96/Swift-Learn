496ee19b24e1e51e68397e0d59c478d35bc097ea
Navinda Lankesh <adilanga@gmail.com>
2021-11-03 18:49:24 +0530

    first commit

---
+++ HelloWorld.swift
@@ -0,0 +1,16 @@
+import Swift
+
+print("Hello World")
+
+print(1 + 2)
+
+let immutable = "You cannot modify this string"
+var mutable = "You can modify this string"
+
+print(immutable)
+print(mutable)
+
+let firstMessage = "Swift is nice."
+let secondMessage = "What do you think?"
+var message = firstMessage + secondMessage
+print(message) // Output
